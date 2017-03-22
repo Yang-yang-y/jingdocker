898eaaaaa0fcfb81205632379fe189208e351e15
张杨 <zhangyang1009@gmail.com>
2017-03-22 18:10:07 +0800

    init jing

---
+++ php/docker-php-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# run composer install
+if [ ! -d "/var/www/html/vendor" ]; then
+  cd /var/www/html
+  composer install
+fi 
+
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+
+exec "$@"
