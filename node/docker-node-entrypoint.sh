0678e678a314dbea14d4e4b4034daaf3272d3403
张杨 <zhangyang1009@gmail.com>
2017-07-25 15:18:10 +0800

    新版nodejs打包

--- node/docker-node-entrypoint.sh
+++ node/docker-node-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -e
-cd /usr/src/app/$SUBPROJECT
+# cd /usr/src/app/$SUBPROJECT
 
-# run composer install
-if [ ! -d "/usr/src/app/$SUBPROJECT/node_modules" ]; then
-  npm install
-fi 
-
-
-# first arg is `-f` or `--some-option`
-# if [ "${1#-}" != "$1" ]; then
-# 	set -- php-fpm "$@"
-# fi
+# # run composer install
+# if [ ! -d "/usr/src/app/$SUBPROJECT/node_modules" ]; then
+#   npm install
+# fi 
 
 
-# exec "$@"
+# # first arg is `-f` or `--some-option`
+# # if [ "${1#-}" != "$1" ]; then
+# # 	set -- php-fpm "$@"
+# # fi
 
-npm run "$SCRIPT"
+
+# # exec "$@"
+
+# npm run "$SCRIPT"
+# npm run start
+cd /usr/src/app/
+sh build.sh $SCRIPT
