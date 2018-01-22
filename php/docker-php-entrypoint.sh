fedf551a8bb129c66a9272d3f2675821469e8e69
admin <623433882@qq.com>
2018-01-22 17:56:41 +0800

    add supervisor

--- php/docker-php-entrypoint.sh
+++ php/docker-php-entrypoint.sh
@@ -7,9 +7,6 @@ if [ ! -d "/var/www/html/vendor" ]; then
   composer install
 fi
 
-su - www-data -c nohup -c "php artisan queue:work --tries=3" 1>>/var/www/html/storage/queue.log 2>&1 &
-
-
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
