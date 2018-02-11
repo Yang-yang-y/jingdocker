43ac5f8eb0c643725cbd987f63517734b23536a0
张杨 <zhangyang1009@gmail.com>
2018-02-11 18:08:06 +0800

    简化环境

--- node/docker-node-entrypoint.sh
+++ node/docker-node-entrypoint.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
 set -e
-# cd /usr/src/app/$SUBPROJECT
 
-# # run composer install
-# if [ ! -d "/usr/src/app/$SUBPROJECT/node_modules" ]; then
-#   npm install
-# fi 
-
-
-# # first arg is `-f` or `--some-option`
-# # if [ "${1#-}" != "$1" ]; then
-# # 	set -- php-fpm "$@"
-# # fi
-
-
-# # exec "$@"
-
-# npm run "$SCRIPT"
-# npm run start
-cd /usr/src/app/
 sh build.sh $SCRIPT
