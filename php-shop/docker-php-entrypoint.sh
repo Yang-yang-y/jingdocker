2f1b418f76780d0b6692167dba6f858fc40a4f8a
caesar chan <caeasar_chan@live.com>
2018-07-25 17:02:59 +0800

    remove composer install

--- php-shop/docker-php-entrypoint.sh
+++ php-shop/docker-php-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-# run composer install
-if [ ! -d "/var/www/xingstation-shop/vendor" ]; then
-  cd /var/www/html
-  composer install
-fi
-
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
